d05bd3ee76e34ad6a74b4fa6ee0e3cfce02a7517
djndl1 <djndl1@gmail.com>
2023-11-20 21:31:13 +0800

    feat(win32/cat): add Caesar cipher implementation

--- win32/src/cat.c
+++ win32/src/cat.c
@@ -19,6 +19,22 @@ static uint8_t buffer[BUFSIZE];
 
 VOID ReportError(LPCTSTR userMessage, DWORD exitCode, BOOL printErrorMessage);
 
+static void caesar_cipher_file(HANDLE in_file, HANDLE out_file, int shift)
+{
+    DWORD bytes_read = 0;
+    DWORD bytes_written = 0;
+    while (ReadFile(in_file, buffer, BUFSIZE, &bytes_read, nullptr)
+           && (bytes_read != 0)) {
+        for (int idx = 0; idx < bytes_read; idx++) {
+            buffer[idx] = (buffer[idx] + shift) % 256;
+        }
+
+        if (!WriteFile(out_file, buffer, bytes_read, &bytes_written, nullptr)) {
+            break;
+        }
+    } 
+}
+
 static void cat_file(HANDLE in_file, HANDLE out_file)
 {
     DWORD bytes_read = 0;
@@ -52,9 +68,27 @@ int main(int argc, char* argv[])
 
     bool dash_s = false; // whether not to show error
 
-    int c = getopt(argc, argv, "s");
-    if (c == 's') {
-        dash_s = true;
+    int c = -1;
+    int shift = 0;
+    while ((c = getopt(argc, argv, "sm:")) != -1) {
+        switch (c) {
+            case 's':
+                dash_s = true;
+                break;
+            case 'm':
+            {
+                shift = atoi(optarg);
+                if (shift <= 0) {
+                    fprintf(stderr, "Failed to convert shift or shift is not positive\n");
+
+                    return EXIT_FAILURE;
+                }
+            }
+            break;
+            case ':':
+                fprintf(stderr, "Missing shift argument\n");
+                return EXIT_FAILURE;
+        }
     }
 
     if (optind == argc) {
@@ -71,7 +105,11 @@ int main(int argc, char* argv[])
                 ReportError(L"Error: file does not exist", 0, true);
             }
         } else {
+            if (shift > 0) {
+                caesar_cipher_file(in_file, hstdout, shift);
+            } else {
                 cat_file(in_file, hstdout);
+            }
 
                 if (GetLastError() != 0 && !dash_s) {
                     ReportError(L"Cat Error.", 0, true);
