4798697a279861917c5f9dd5d4b78336415d6bf2
djndl1 <djndl1@gmail.com>
2023-11-30 21:42:47 +0800

    feat(win32/console ctrl): console control handler

---
+++ win32/src/console_ctrl.c
@@ -0,0 +1,46 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <wchar.h>
+#include <stdatomic.h>
+
+static atomic_bool exit_flag = false;
+
+static BOOL WINAPI Handler(DWORD cntrlEvent)
+{
+    exit_flag = true;
+
+    switch (cntrlEvent) {
+        case CTRL_C_EVENT:
+            wprintf(L"Ctrl-C Leaving in <= 5 seconds\n");
+            Sleep(4000);
+            wprintf(L"Leaving handler in 1 second or less\n");
+            return TRUE;
+        case CTRL_CLOSE_EVENT:
+            wprintf(L"Close event. Leaving in <= 5 seconds\n");
+            Sleep(4000);
+            wprintf(L"Leaving handler in 1 second or less\n");
+            return TRUE;
+        default:
+            wprintf(L"Event %d. Leaving in <= 5 seconds\n");
+            Sleep(4000);
+            wprintf(L"Leaving handler in 1 second or less\n");
+            return TRUE;
+    }
+}
+
+int wmain(int argc, wchar_t* argv[])
+{
+    SetConsoleCtrlHandler(Handler, TRUE);
+
+    while (!exit_flag) {
+        Sleep(5000);
+        Beep(1000, 250);
+    }
+
+    wprintf(L"Stopping the program as requested\n");
+    return EXIT_SUCCESS;
+}
