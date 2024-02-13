de571dd5750cfef53922a0887bc665cbe979aea7
djndl1 <djndl1@gmail.com>
2024-02-13 09:10:45 +0000

    feat(concurrency): completed SimpleThreadWin32

--- concurrency/SimpleThreadWin32.cc
+++ concurrency/SimpleThreadWin32.cc
@@ -4,24 +4,45 @@
 #include <cinttypes>
 #include <windows.h>
 #include <process.h>
+#include <memory>
 
 using std::cout;
+using std::wcerr;
+using std::wstring;
+
+template <typename T>
+struct LocalFreeDeleter
+{
+    public:
+        void operator()(T *obj) const
+        {
+            ::LocalFree(obj);
+        }
+};
 
 static void print_error(const wchar_t *message, const char *filename, int linenum)
 {
+    DWORD err_code = ::GetLastError();
 
+    wchar_t *errorMessage = nullptr;
+    ::FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+                     nullptr, err_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                     (LPWSTR)&errorMessage, 0, nullptr);
+    std::unique_ptr<wchar_t, LocalFreeDeleter<wchar_t>> err_msg_ptr(errorMessage);
+
+    std::wcerr << message << L" failed at line " << linenum << L" in " << filename
+               << L" with error " << err_code << L":" << err_msg_ptr.get();
+
+    ::exit(err_code);
 }
 
 __stdcall unsigned run_simple_thread(void *args)
 {
-    //std::cout << "Thread " << reinterpret_cast<uintptr_t>(args) << " Running\n";
-
-    //std::cout << "Thread " << (args) << " Running\n";
     uintptr_t id = reinterpret_cast<uintptr_t>(args);
-    //::printf("Thread %" PRIuPTR " running\n", id);
-    ::printf("Thread %d running\n", (int)id);
+    std::string msg = "Thread " + std::to_string(id) + " Running\n";
+    std::cout << msg;
 
-    return 0;
+    return static_cast<unsigned>(id);
 }
 
 int main()
@@ -42,8 +63,16 @@ int main()
 
     }
     DWORD rc = ::WaitForMultipleObjects(n_threads, thread_handles, TRUE, INFINITE);
+    if (rc < WAIT_OBJECT_0 || rc > (WAIT_OBJECT_0 + n_threads)) {
+        print_error(L"WaitForMultipleObjects failed at", __FILE__, __LINE__);
+    }
 
     for (HANDLE handle : thread_handles) {
+        DWORD exit_code;
+        if (!GetExitCodeThread(handle, &exit_code)) {
+            print_error(L"GetExitCodeThread failed at", __FILE__, __LINE__);
+        }
+        std::cout << "Thread " + std::to_string(exit_code) + " Exited.\n";
         CloseHandle(handle);
     }
 
