50298573d70c2f053491b62a27b899e1df4814ac
djndl1 <djndl1@gmail.com>
2024-11-11 22:29:58 +0800

    fix(unix/fileio): error if the file exists

--- unix/src/file_desc.c
+++ unix/src/file_desc.c
@@ -35,7 +35,7 @@ file_desc_result_t file_open(const char *pathname, int flags)
 file_desc_result_t file_create_impl(const char *pathname, int flags, mode_t modes, bool temp)
 {
     file_desc_result_t result = { .fd = { -1 }, .error = 0 };
-    int create_flags = flags | (temp ? O_TMPFILE : O_CREAT);
+    int create_flags = flags | (temp ? O_TMPFILE : O_CREAT) | O_EXCL;
 
     int fd = open(pathname, create_flags, modes);
     if (fd == -1) {
