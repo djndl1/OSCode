4fc6ca600aa0ee54908b30c9f8cead5a3ac99d4b
djndl1 <djndl1@gmail.com>
2024-12-24 22:43:20 +0800

    feat(win32): winheap_allocator, local_allocator, global_allocator

--- win32/include/winallocator.h
+++ win32/include/winallocator.h
@@ -11,7 +11,46 @@ extern "C" {
 
 extern const mem_allocator *const process_heap_allocator;
 
-winhandle heap_allocator_handle(const mem_allocator *const allocator);
+winhandle winheap_allocator_handle(const mem_allocator *const allocator);
+
+
+typedef struct winheap_creation_options {
+    size_t initial_size;
+    size_t maximum_size;
+    bool use_seh_exception;
+    bool lockless;
+    bool code_executable_from_heap;
+} winheap_creation_options;
+
+typedef struct winheap_creation_result {
+    winstatus status;
+    mem_allocator allocator;
+} winheap_creation_result;
+
+winheap_creation_result winheap_allocator_create(winheap_creation_options options);
+
+winstatus winheap_allocator_destroy(mem_allocator *self);
+
+typedef struct winheap_size_result {
+    winstatus status;
+    size_t size;
+} winheap_size_result;
+
+winheap_size_result winheap_allocator_largest_free_block_size(const mem_allocator *const self);
+
+winheap_size_result winheap_allocator_committed_size(const mem_allocator* const self);
+
+winheap_size_result winheap_allocator_allocated_size(const mem_allocator* const self);
+
+winheap_size_result winheap_allocator_reserved_size(const mem_allocator* const self);
+
+winheap_size_result winheap_allocator_max_reserved_size(const mem_allocator* const self);
+
+winheap_size_result winheap_allocator_query_block_size(const mem_allocator* const self, const void* mem);
+
+extern const mem_allocator *const global_allocator;
+
+extern const mem_allocator *const local_allocator;
 
 #ifdef __cplusplus
 }
